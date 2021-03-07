065bee743f08ef420d2864feeab4735aab0b9508
Andreas Marsh <marsh.andreas.s@gmail.com>
2021-03-07 15:41:20 -0500

    Initial Commit

---
+++ Inspiration Generator/Inspiration_GeneratorApp.swift
@@ -0,0 +1,17 @@
+//
+//  Inspiration_GeneratorApp.swift
+//  Inspiration Generator
+//
+//  Created by Andreas Marsh on 3/7/21.
+//
+
+import SwiftUI
+
+@main
+struct Inspiration_GeneratorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
